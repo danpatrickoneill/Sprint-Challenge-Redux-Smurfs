f5370e4608e5e4910c6a330df93c111a5f852aee
danpatrickoneill <dan47573@gmail.com>
2019-04-12 11:58:50 -0400

    Added form to SmurfList component

--- smurfs/src/components/SmurfList.js
+++ smurfs/src/components/SmurfList.js
@@ -4,19 +4,60 @@ import { connect } from "react-redux";
 import { getSmurfs } from "../actions";
 
 class SmurfList extends React.Component {
-  state = {};
+  state = {
+    name: "",
+    age: "",
+    height: ""
+  };
 
   componentDidMount() {
     this.props.getSmurfs();
   }
 
+  handleChanges = e => {
+    console.log(this.state);
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  submitSmurf = e => {
+    e.preventDefault();
+  };
+
   render() {
-    console.log(this.props);
     return (
       <div className="smurfList">
         {this.props.smurfs.map((smurf, index) => (
-          <p key={index}>{smurf.name}</p>
+          <p
+            key={index}
+            style={{ fontSize: `${parseInt(smurf.height) * 4}px` }}
+          >
+            {smurf.name}, {smurf.age}
+          </p>
         ))}
+        <form onSubmit={this.submitSmurf} className="addSmurf">
+          <h2>Add your favorite Smurf!</h2>
+          <input
+            onChange={this.handleChanges}
+            type="text"
+            name="name"
+            placeholder="Name"
+          />
+          <input
+            onChange={this.handleChanges}
+            type="text"
+            name="age"
+            placeholder="Age"
+          />
+          <input
+            onChange={this.handleChanges}
+            type="text"
+            name="height"
+            placeholder="Height"
+          />
+          <button>Add Smurf!</button>
+        </form>
       </div>
     );
   }
